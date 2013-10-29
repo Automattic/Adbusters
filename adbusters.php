7343f25e36795c5f1ddd1a9bbb95253515674d40
Paul Gibbs <paul@byotos.com>
2013-10-29 13:48:09 +0000

    Initial commit

---
+++ adbusters.php
@@ -0,0 +1,65 @@
+<?php
+/*
+Plugin Name: Adbusters
+Plugin URI: https://github.com/Automattic/Adbusters
+Description: Iframe busters for common ad providers.
+Version: 1.0
+Requires at least: 3.7
+Tested up to: 3.7.20
+License: GPLv3
+Author: Paul Gibbs, Mohammad Jangda, Automattic
+Author URI: http://automattic.com/
+Text Domain: adbusters
+
+"Adbusters"
+Copyright (C) 2013 Automattic
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+// TODO: some are strpos (e.g. DART), some are exact matches so need to account for those. Majority will be exact matches.
+
+function wpcom_vip_maybe_load_ad_busters() {
+
+	$ad_busters = array(
+		'/atlas/atlas_rm.htm',            // Atlas
+		'/doubleclick/DARTIframe.html',   // Google - DoubleClick
+		'/eyeblaster/addineyeV2.html',    // MediaMind - EyeBlaster
+		'/eyereturn/eyereturn.html',      // ? - EyeReturn
+		'/pointroll/PointRollAds.htm',    // PointRoll
+		'/unicast/unicastIFD.html',       // Unicast
+		'/adinterax/adx-iframe-v2.html',  // Yahoo - AdInterax
+	);
+
+	// To ignore an ad network, use this filter and return an array containing the values of $ad_busters to not load
+	$block_ads  = apply_filters( 'wpcom_vip_maybe_load_ad_busters', array() );
+	$ad_busters = array_diff( $ad_busters, $block_ads );
+
+	// Do we have a request for a supported network?
+	$request = parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH );
+	$index   = array_search( $request, $ad_busters );
+	if ( false === $index )
+		return;
+
+	$file = plugin_dir_path( __FILE__ ) . 'templates/' . $ad_busters[$index];
+
+	// Spit out the template
+	header( 'Content-type: text/html' );
+	readfile( $file );
+
+	exit;
+}
+add_action( 'init', 'wpcom_vip_maybe_load_ad_busters', -1 );
