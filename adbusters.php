91e7b4694c57b78189221dda9e5f45b121d4d7ff
Natebot <natebot@gmail.com>
2013-12-14 17:19:23 -0800

    Explicitly select which adbusters to support
    introducing a new filter ‘wpcom_vip_maybe_load_ad_busters_whitelist’
    that allows selection which adbusters to be served.

--- adbusters.php
+++ adbusters.php
@@ -67,10 +67,20 @@ function wpcom_vip_maybe_load_ad_busters() {
 		'_uac/adpage.html',                  // AOL - atwola.com
 	);
 
+	// To only support a specific ad network, user this filter and return an array containing the values of $ad_busters to load
+	$whitelist_ads = (array) apply_filters( 'wpcom_vip_maybe_load_ad_busters_whitelist', $ad_busters );
+	$ad_busters = array_filter( $ad_busters, function( $ad_buster_path ) use ( $whitelist_ads ){
+		return in_array( $ad_buster_path, $whitelist_ads ); 
+	} );
+	
 	// To ignore an ad network, use this filter and return an array containing the values of $ad_busters to not load
 	$block_ads  = apply_filters( 'wpcom_vip_maybe_load_ad_busters', array() );
 	$ad_busters = array_diff( $ad_busters, $block_ads );
 	$ad_paths   = $ad_busters;
+	
+	// do we have any supported networks?
+	if ( empty( $ad_paths ) )
+		return;
 
 	// If your ads need to be served from example.com/some/subfolder/*, pass "some/subfolder" to this filter
 	$path = explode( '/', apply_filters( 'wpcom_vip_ad_busters_custom_path', '' ) );
