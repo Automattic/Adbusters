d98bff730124a9d3a26547b9d5a0849255274614
Kailey Lampert <trepmal@gmail.com>
2021-06-24 14:27:24 -0700

    phpcs fixes for adbusters.php

--- adbusters.php
+++ adbusters.php
@@ -1,60 +1,70 @@
 <?php
-/*
-Plugin Name: Adbusters
-Plugin URI: https://github.com/Automattic/Adbusters
-Description: Iframe busters for popular ad networks.
-Version: 1.0.7
-Requires at least: 3.7
-License: GPLv3
-Author: Paul Gibbs, Mohammad Jangda, Automattic
-Author URI: http://automattic.com/
-Text Domain: adbusters
+/**
+ * Plugin Name: Adbusters
+ * Plugin URI: https://github.com/Automattic/Adbusters
+ * Description: Iframe busters for popular ad networks.
+ * Version: 1.0.7
+ * Requires at least: 3.7
+ * License: GPLv3
+ * Author: Paul Gibbs, Mohammad Jangda, Automattic
+ * Author URI: http://automattic.com/
+ * Text Domain: adbusters
+ *
+ * @package Adbusters
+ *
+ * "Adbusters"
+ * Copyright (C) 2013 Automattic
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ * @phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedFunctionFound -- Legacy generic `wpcom_vip_` prefix in use
+ * @phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedHooknameFound -- Legacy generic `wpcom_vip_` prefix in use
+ */
 
-"Adbusters"
-Copyright (C) 2013 Automattic
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License version 3 as published by
-the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see http://www.gnu.org/licenses/.
-*/
-
-// Exit if accessed directly
+// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+/**
+ * Get array of Adbuster templates.
+ *
+ * @return array Template paths.
+ */
 function wpcom_vip_get_ad_busters_array() {
 	return array(
-		'_uac/adpage.html',                   // AOL - atwola.com
-		'adcom/aceFIF.html',                  // Advertising.com (ad.com)
-		'adinterax/adx-iframe-v2.html',       // AdInterax
-		'atlas/atlas_rm.htm',                 // Atlas
-		'blogads/iframebuster-4.html',        // BlogAds
-		'checkm8/CM8IframeBuster.html',       // CheckM8
-		'comscore/cs-arIframe.htm',           // comScore
-		'doubleclick/adx_iframe.html',        // Google - DoubleClick
-		'doubleclick/DARTIframe.html',        // Google - DoubleClick
-		'doubleclick/fif.html',               // Flite
-		'doubleclick/TLIframe.html',          // TripleLift
-		'flite/fif.html',                     // Flite
-		'gumgum/iframe_buster.html',          // gumgum
-		'jpd/jpxdm.html',                     // Jetpack Digital
-		'knorex/knorex_psf.html',             // Knorex
-		'linkstorm/linkstorm_certified.html', // Linkstorm
-		'oggifinogi/oggiPlayerLoader.htm',    // Collective - OggiFinogi
-		'pictela/Pictela_iframeproxy.html',   // AOL - Pictela
-		'pointroll/PointRollAds.htm',         // PointRoll
-		'rubicon/rp-smartfile.html',          // Rubicon
-		'saymedia/iframebuster.html',         // Say Media
-		'xaxis/InfinityIframe.html',          // Xaxis
+		'_uac/adpage.html',                   // AOL - atwola.com.
+		'adcom/aceFIF.html',                  // Advertising.com (ad.com).
+		'adinterax/adx-iframe-v2.html',       // AdInterax.
+		'atlas/atlas_rm.htm',                 // Atlas.
+		'blogads/iframebuster-4.html',        // BlogAds.
+		'checkm8/CM8IframeBuster.html',       // CheckM8.
+		'comscore/cs-arIframe.htm',           // comScore.
+		'doubleclick/adx_iframe.html',        // Google - DoubleClick.
+		'doubleclick/DARTIframe.html',        // Google - DoubleClick.
+		'doubleclick/fif.html',               // Flite.
+		'doubleclick/TLIframe.html',          // TripleLift.
+		'flite/fif.html',                     // Flite.
+		'gumgum/iframe_buster.html',          // gumgum.
+		'jpd/jpxdm.html',                     // Jetpack Digital.
+		'knorex/knorex_psf.html',             // Knorex.
+		'linkstorm/linkstorm_certified.html', // Linkstorm.
+		'oggifinogi/oggiPlayerLoader.htm',    // Collective - OggiFinogi.
+		'pictela/Pictela_iframeproxy.html',   // AOL - Pictela.
+		'pointroll/PointRollAds.htm',         // PointRoll.
+		'rubicon/rp-smartfile.html',          // Rubicon.
+		'saymedia/iframebuster.html',         // Say Media.
+		'xaxis/InfinityIframe.html',          // Xaxis.
 	);
 }
 
@@ -70,26 +80,26 @@ function wpcom_vip_get_ad_busters_array() {
 function wpcom_vip_maybe_load_ad_busters() {
 	$ad_busters = wpcom_vip_get_ad_busters_array();
 
-	// To only support a specific ad network, use this filter and return an array containing the values of $ad_busters to load
+	// To only support a specific ad network, use this filter and return an array containing the values of $ad_busters to load.
 	$whitelist_ads = (array) apply_filters( 'wpcom_vip_ad_busters_whitelist', $ad_busters );
 	$ad_busters    = array_intersect( $ad_busters, $whitelist_ads );
 
-	// To ignore an ad network, use this filter and return an array containing the values of $ad_busters to not load
+	// To ignore an ad network, use this filter and return an array containing the values of $ad_busters to not load.
 	$block_ads  = apply_filters( 'wpcom_vip_maybe_load_ad_busters', array() );
 	$ad_busters = array_diff( $ad_busters, $block_ads );
 	$ad_paths   = $ad_busters;
 
-	// nothing to do if there are no paths after whitelisting and blocking
+	// nothing to do if there are no paths after whitelisting and blocking.
 	if ( empty( $ad_paths ) ) {
 		return;
 	}
 
-	// If your ads need to be served from example.com/some/subfolder/*, pass "some/subfolder" to this filter
+	// If your ads need to be served from example.com/some/subfolder/*, pass "some/subfolder" to this filter.
 	$path = explode( '/', apply_filters( 'wpcom_vip_ad_busters_custom_path', '' ) );
 	$path = array_filter( array_map( 'sanitize_title', $path ) );
 	$path = implode( '/', $path );
 
-	// Make sure both the ends of the path have slashes
+	// Make sure both the ends of the path have slashes.
 	$path = _wpcom_vip_leadingslashit( $path );
 	$path = trailingslashit( $path );
 
@@ -97,17 +107,17 @@ function wpcom_vip_maybe_load_ad_busters() {
 		function( $ad_file ) use ( $path ) {
 			return "{$path}{$ad_file}";
 		},
-		$ad_busters 
+		$ad_busters
 	);
 
 	// Do we have a request for a supported network?
-	$request = parse_url( isset( $_SERVER['REQUEST_URI'] ) ? $_SERVER['REQUEST_URI'] : '', PHP_URL_PATH );
+	$request = wp_parse_url( filter_input( INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_STRING ), PHP_URL_PATH );
 	$index   = array_search( $request, $ad_busters );
 	if ( false === $index ) {
 		return;
 	}
 
-	// Spit out the template
+	// Spit out the template.
 	$file = plugin_dir_path( __FILE__ ) . 'templates/' . $ad_paths[ $index ];
 	if ( ! file_exists( $file ) ) {
 		return;
